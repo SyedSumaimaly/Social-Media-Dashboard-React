2dbfa68bf946cae7e25d31d63a7d51e2a49ec312
SyedSumaimaly <syedsumaimaly08@gmail.com>
2023-10-10 08:42:10 -0700

    Adding Navbar & Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Footer() {
+    return (
+       <div className="container-fluid text-center bg-dark">
+        <div className='py-2'>
+            <p className='finfo'>Copyright © 2020 All Rights Reserved by Syed Sumaim Ali</p>
+        </div>
+       </div>
+    )
+}
+
+export default Footer
